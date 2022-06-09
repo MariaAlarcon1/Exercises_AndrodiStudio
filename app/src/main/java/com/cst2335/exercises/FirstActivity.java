7810f126e98132bc4cad598068b6c8264b6d6932
Maria Alarcon <maria.alarcong1@gmail.com>
2022-06-08 20:53:13 -0400

    Class explanation 06/08/2022

--- app/src/main/java/com/cst2335/exercises/FirstActivity.java
+++ app/src/main/java/com/cst2335/exercises/FirstActivity.java
@@ -29,10 +29,11 @@ public class FirstActivity extends AppCompatActivity {
         // Before this function, the screen is empty.
         setContentView(R.layout.activity_main);
 
+        //STEP 1:
         ListView myList = findViewById(R.id.theListView);
         //Line 37 is the same as lines 35 and 36:
         //ListAdapter myAdapter = new MyListAdapter();
-        //myList.setAdapter( myAdapter);
+        //STEP 2: myList.setAdapter( myAdapter);
         myList.setAdapter( myAdapter = new MyListAdapter());
 
         Button addButton = findViewById(R.id.addButton);
@@ -43,21 +44,35 @@ public class FirstActivity extends AppCompatActivity {
 
     }
 
+    // Every class that implements BaseAdapter needs to implements 4 methods
     private class MyListAdapter extends BaseAdapter {
 
-        public int getCount() { return elements.size();}
+        // returns the number of elements
+        public int getCount() { return elements.size();} //elements is a list
 
+        //what should be displayed on each of the views of the list
         public Object getItem(int position) { return "This is row " + position; }
 
+        // for database --> keeps track of the ID of the DB
         public long getItemId(int position) { return (long) position; }
 
+
+        // defines how each view will look like int the list. work together with getItem(). Returns the item on the vire
         public View getView(int position, View old, ViewGroup parent)
         {
+            // inflates allows to define our view
             LayoutInflater inflater = getLayoutInflater();
 
+            /**
+             * row_layout ->
+             */
             //make a new row:
             View newView = inflater.inflate(R.layout.row_layout, parent, false);
 
+            /**
+             * newView.find... --> find all the items inside the layout from (row_layout)
+             * to be able to show all of the items using --> tView,setText
+             */
             //set what the text should be for this row:
             TextView tView = newView.findViewById(R.id.textGoesHere);
             tView.setText( getItem(position).toString() );
